14c768b0baa04b83a8328cbcfec107d27e37ec0d
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-03 10:24:18 +0530

    Time: 2 ms (72.59%), Space: 59 MB (59.01%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,34 @@
+//Brute force tc=O(n^2) SC=O(1)
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int max=0;
+//         for(int i=0;i<prices.length-1;i++){
+//             for(int j=i+1;j<prices.length;j++){
+//                 max=Math.max(max,prices[j]-prices[i]);
+//             }
+//         }
+//         return max;
+//     }
+// }
+
+//optimized approach tc=O(n) SC=O(1) Sliding window approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lsf = Integer.MAX_VALUE; // least so far
+        int op = 0; // overall profit
+        int pist = 0; // profit if sold today
+        for(int i = 0; i < prices.length; i++){
+// if we found new buy value which is more smaller then previous one
+            if(prices[i] < lsf){ 
+                lsf = prices[i]; // update our least so far
+            }
+// calculating profit if sold today by, Buy - sell            
+            pist = prices[i] - lsf; 
+// if pist is more then our previous overall profit
+            if(op < pist){ 
+                op = pist; // update overall profit
+            }
+        }
+        return op; // return op 
+    }
+}
