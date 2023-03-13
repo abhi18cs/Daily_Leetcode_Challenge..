1b4308cdd94c4c2eeb400e84ee9adba311ae2ca3
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-13 10:37:20 +0530

    Time: 1 ms (100.00%), Space: 41.3 MB (50.33%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,22 +1,25 @@
-class Solution {
-    public String addBinary(String a, String b) {
-        return new java.math.BigInteger(a,2).add(new java.math.BigInteger(b,2)).toString(2);
-    }
-}
-
+//Approach-1 Using Pre-Defined Functions TC=O(n) Sc=O(1)
 // class Solution {
 //     public String addBinary(String a, String b) {
-//         StringBuilder res=new StringBuilder();
-//         int add=0;
-//         for(int i=1;i<=Math.max(a.length(),b.length());i++){
-//             if(i<=a.length())
-//                 add+=a.charAt(a.length()-i)-'0';
-//             if(i<=b.length())
-//                 add+=b.charAt(b.length()-i)-'0';
-//             res.append(add%2);
-//             add/=2;
-//         }
-//         if(add>0) res.append(1);
-//         return res.reverse().toString();
+//         return new java.math.BigInteger(a,2).add(new java.math.BigInteger(b,2)).toString(2);
 //     }
 // }
+
+//Appraoch-2 Using StringBuilder TC=O(n) Sc=O(1)
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
+        if(carry != 0) res.append(carry);
+        return res.reverse().toString();
+    }
+}
