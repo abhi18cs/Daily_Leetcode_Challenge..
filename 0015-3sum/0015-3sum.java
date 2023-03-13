cfb492bb81b64dd8b63f462709143ad97ee6964e
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-13 10:18:39 +0530

    Time: 35 ms (90.19%), Space: 49.9 MB (51.17%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,26 +1,53 @@
-//brute force using 3 loops tc=O(n3)sc=O(3*k)
-//using hashing tc=O(n2*logm)sc=O(m)+O(n)
-//optimized approach using Two pointers tc=O(n2) sc=O(m)[auxiallry]
+//Approach-1 brute force using 3 loops tc=O(n3)sc=O(3*k)
+
+//Approach-2 using hashing tc=O(n2*logm)sc=O(m)+O(n)
+// class Solution {
+//     public List<List<Integer>> threeSum(int[] nums) {
+//         Set<List<Integer>> resultSet = new HashSet();
+//         Arrays.sort(nums);
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i < nums.length; i++) map.put(nums[i], i);
+
+//         for(int i = 0; i < nums.length - 2 && nums[i] <= 0;){
+//             for (int j = i + 1; j < nums.length && nums[i] + nums[j] <= 0;) {
+//                 int value = 0 - nums[i] - nums[j];
+//                 //if (value < 0) break;
+//                 if (value < 0) return new ArrayList<>(resultSet);
+//                 if (value < nums[j]) break;
+//                 if (map.containsKey(value) && map.get(value) > j)
+//                     resultSet.add(Arrays.asList(nums[i], nums[j], value));
+//                 j++;
+//                 while(j < nums.length && nums[j] == nums[j - 1]) j++;
+//             }
+//             i++;
+//             while(i < nums.length - 2 && nums[i] == nums[i - 1]) i++;
+//         }
+//         return new ArrayList<>(resultSet) ;
+//     }
+// }
+
+// //Approach-3 optimized approach using Two pointers Tc=O(N * LogN + N * N) Sc=O(N + LogN)[auxiallry]
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res=new LinkedList<>();
-        for(int i=0;i<nums.length-2;i++){
+        Arrays.sort(nums);//O(nlogn)
+        int n=nums.length;
+        List<List<Integer>> result=new LinkedList<>();
+        for(int i=0;i<n-2;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-                int lo=i+1,hi=nums.length-1,sum=0-nums[i];
-                //2 pointers
+                int lo=i+1,hi=n-1,sum=0-nums[i];
+                //2pointers
                 while(lo<hi){
                     if(nums[lo]+nums[hi]==sum){
-                        res.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
+                        result.add(Arrays.asList(nums[i],nums[lo],nums[hi]));
                         while(lo<hi && nums[lo]==nums[lo+1]) lo++;
                         while(lo<hi && nums[hi]==nums[hi-1]) hi--;
                         lo++;hi--;
                     }
-                    else if((nums[lo]+nums[hi])<sum) lo++;
+                    else if(nums[lo]+nums[hi]<sum) lo++;
                     else hi--;
                 }
             }
         }
-        return res;
+            return result;
     }
 }
