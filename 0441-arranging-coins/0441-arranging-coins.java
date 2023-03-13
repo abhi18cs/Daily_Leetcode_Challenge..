af230da9f5a511ee3090b57f69d2f705e3571d80
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-13 11:17:57 +0530

    Time: 2 ms (84.23%), Space: 40.7 MB (14.13%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,25 +1,26 @@
 //Approach-1 Using Iterative Tc=O(sqrt(n)) Sc=O(1)
-class Solution {
-    public int arrangeCoins(int n) {
-    int i=1;
-        while(n>0){
-            i++;
-            n=n-i;
-        }
-        return i-1;
-    }
-}
-
 // class Solution {
 //     public int arrangeCoins(int n) {
-//         long l=1,h=n;
-//         while(l<=h){
-//             long m=l+(h-l)/2;
-//             long coinUsed=m*(m+1)/2;
-//             if(coinUsed==n) return (int)m;
-//             else if(coinUsed>n) h=m-1;
-//             else l=m+1;
-//         }
-//         return (int)h;
+//         int i = 1; // which row we are on
+// 		while(n > 0){ // checking to see if we have used all our coins
+// 			i++; // increasing our row
+// 			n = n-i; // adding coins to our row
+// 		}
+// 		return i-1; // we return our current row minus one because the last row is our completed row
 //     }
 // }
+
+//Approach-2 Using BinarySearch Tc=O(log(n)) Sc=O(1)
+class Solution {
+    public int arrangeCoins(int n) {
+        long l=1,h=n;
+        while(l<=h){
+            long m=l+(h-l)/2;
+            long coinUsed=m*(m+1)/2;
+            if(coinUsed==n) return (int)m;
+            else if(coinUsed>n) h=m-1;
+            else l=m+1;
+        }
+        return (int)h;
+    }
+}
