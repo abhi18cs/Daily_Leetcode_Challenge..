c31a03b55e389c996e1ad3dfa8100bbcb468c53c
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-18 10:28:51 +0530

    Time: 52 ms (24.14%), Space: 67.5 MB (19.54%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,12 +1,15 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-       Arrays.sort(nums);
-	   // sort in wave format
-        for(int i = 0;i<nums.length-1;i+=2){
-            int temp = nums[i];
-            nums[i] = nums[i+1];
-            nums[i+1] = temp;
+        Arrays.sort(nums); // Sort the array to ensure distinct integers
+        int[] res = new int[nums.length];
+        int left = 0, right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                res[i] = nums[left++];
+            } else {
+                res[i] = nums[right--];
+            }
         }
-        return nums;
+        return res;
     }
 }
