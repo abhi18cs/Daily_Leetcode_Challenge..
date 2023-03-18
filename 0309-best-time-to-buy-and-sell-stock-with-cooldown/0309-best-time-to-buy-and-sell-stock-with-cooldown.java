96f6af850316b38e96ad819ec86eec6ccc44c7b3
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-18 10:11:11 +0530

    Time: 1 ms (73.42%), Space: 40.9 MB (42.47%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,13 +1,22 @@
+//Tc=O(n) Sc=O(1)
 class Solution {
     public int maxProfit(int[] prices) {
-        if(prices == null || prices.length <= 1) return 0;
-        int b0 = -prices[0], b1 = b0;
-        int s0 = 0, s1 = 0, s2 = 0;
-        for(int i = 1; i < prices.length; i++) {
-            b0 = Math.max(b1, s2 - prices[i]);
-            s0 = Math.max(s1, b1 + prices[i]);
-            b1 = b0; s2 = s1; s1 = s0; 
+        if (prices == null || prices.length <= 1) return 0;
+        int buy = -prices[0];
+        int sell = 0;
+        int prevSell = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // buy[i] = Math.max(sell[i-2]-prices[i], buy[i-1])
+            // sell[i-2]-prices[i] -> Buy after a 1 day cooldown
+            // buy[i-1] -> cooldown
+            buy = Math.max(prevSell - prices[i], buy);
+            // sell[i-1];
+            prevSell = sell;
+            // sell[i] = Math.max(buy[i]+prices, sell[i-1])
+            // buy[i]+prices -> sell the stock.
+            // sell[i-1] -> cooldown
+            sell = Math.max(buy + prices[i], sell);
         }
-        return s0;
+        return sell;
     }
 }
