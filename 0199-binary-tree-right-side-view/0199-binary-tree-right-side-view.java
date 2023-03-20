ef18c535dfbe15285f358487e80ad4529c325afd
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-20 09:25:21 +0530

    Time: 1 ms (70.18%), Space: 40.8 MB (79.58%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,18 +1,46 @@
 //Approach-1 Using DFS(RECURSIVE) 
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        ArrayList<Integer> ans=new ArrayList<>();
-        helper(root,ans,0);
-        return ans;
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         ArrayList<Integer> ans=new ArrayList<>();
+//         helper(root,ans,0);
+//         return ans;
         
-    }
-    public void helper(TreeNode root,List<Integer> list,int depth){
-        if(root!=null){
-            if(depth==list.size()){
-                list.add(root.val);
+//     }
+//     public void helper(TreeNode root,List<Integer> list,int depth){
+//         if(root!=null){
+//             if(depth==list.size()){
+//                 list.add(root.val);
+//             }
+//             helper(root.right,list,depth+1);
+//             helper(root.left,list,depth+1);
+//         }
+//     }
+// }
+
+//Approach-2 BFS(QUEUE/ITERATIVE)
+class Solution{
+    public List<Integer> rightSideView(TreeNode root){
+        ArrayList<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            TreeNode node=null;
+            while(size>0){
+                node=queue.poll();
+                if(node.left!=null){
+                    //add in the queue
+                    queue.offer(node.left);
+                }
+                if(node.right!=null){
+                    //add in the queue
+                    queue.offer(node.right);
+                }
+                size--;
             }
-            helper(root.right,list,depth+1);
-            helper(root.left,list,depth+1);
+            ans.add(node.val);
         }
+        return ans;
     }
 }
