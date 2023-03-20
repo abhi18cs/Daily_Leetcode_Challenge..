c4c619657a893657cb8939ceb11cb01bfa4f7be4
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-20 10:09:56 +0530

    Time: 4 ms (99.68%), Space: 50.6 MB (58.49%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,15 +1,52 @@
+
+//Approach-1 Using 2 Pointers Tc=O(nlogn) Sc=(1)
+// class Solution {
+//     public int numRescueBoats(int[] people, int limit) {
+//         int count=0;
+//         Arrays.sort(people);
+//         int i=0,j=people.length-1;
+//         while(i<=j){
+//             if(people[i]+people[j]<=limit){
+//                 i++;j--;
+//             }
+//             else j--;
+//             count++;
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        int count=0;
-        Arrays.sort(people);
-        int i=0,j=people.length-1;
-        while(i<=j){
-            if(people[i]+people[j]<=limit){
-                i++;j--;
-            }
-            else j--;
-            count++;
+        int[] map = new int[limit + 1];
+        
+        for(int x : people){
+            map[x]++;
         }
-        return count;
+        
+        int i = 0;
+        int j = limit;
+        int numPeople = 0;
+        
+        while(i <= j){
+            while(map[i] == 0 && i <= j){
+                i++;
+            }
+            while(map[j] == 0 && i <= j){
+                j--;
+            }
+            if(i + j <= limit){
+                if(map[j] > 0)
+                   map[j]--;
+                if(map[i] > 0)
+                   map[i]--;
+            }
+            else{
+                if(map[j] > 0)
+                   map[j]--;
+            }
+            numPeople++;
+        }
+        return numPeople - 1;
     }
 }
