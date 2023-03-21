498486e0cf9e034b2ae0ffacc0707d1fede6629f
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-21 09:03:53 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (95.64%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,11 +1,31 @@
 //Approach-1 Using Recursion Tc=O(n) Sc=O()
+// class Solution {
+//     ArrayList<Integer> list=new ArrayList<>();
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         if(root==null) return list;
+//         list.add(root.val);
+//         preorderTraversal(root.left);
+//         preorderTraversal(root.right);
+//         return list;
+//     }
+// }
+
+//Approach-2 Using Iteratively Tc=O(n) Sc=O()
 class Solution {
-    ArrayList<Integer> list=new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root==null) return list;
-        list.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
-        return list;
+    List<Integer> result = new ArrayList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.add(p.val);  // Add before going to children
+            p = p.left;
+        } else {
+            TreeNode node = stack.pop();
+            p = node.right;   
+        }
+    }
+    return result;
     }
 }
