71fb86084bef6888a0d4ba4527c04568a01d7083
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-21 09:05:21 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (46.78%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,11 +1,32 @@
+//Approach-1 Using Recursion Tc=O(n) Sc=O()
+// class Solution {
+//     ArrayList<Integer> list=new ArrayList<>();
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         if(root!=null){
+//             postorderTraversal(root.left);
+//             postorderTraversal(root.right);
+//             list.add(root.val);
+//         }
+//         return list;
+//     }
+// }
+
+//Approach-2 Using Iteratively Tc=O(n) Sc=O()
 class Solution {
-    ArrayList<Integer> list=new ArrayList<>();
     public List<Integer> postorderTraversal(TreeNode root) {
-        if(root!=null){
-            postorderTraversal(root.left);
-            postorderTraversal(root.right);
-            list.add(root.val);
+    LinkedList<Integer> result = new LinkedList<>();
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    TreeNode p = root;
+    while(!stack.isEmpty() || p != null) {
+        if(p != null) {
+            stack.push(p);
+            result.addFirst(p.val);  // Reverse the process of preorder
+            p = p.right;             // Reverse the process of preorder
+        } else {
+            TreeNode node = stack.pop();
+            p = node.left;           // Reverse the process of preorder
         }
-        return list;
+    }
+    return result;
     }
 }
