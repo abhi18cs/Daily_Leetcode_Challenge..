67ec446729cd3e0dbce55e551a4f2278a03a84f2
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-22 16:21:50 +0530

    Time: 5 ms (20.16%), Space: 52.5 MB (58.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,37 @@
-class Solution {
-    public int maxArea(int[] height) {
-    int i = 0, j = height.length - 1, maxArea = 0;
-        while (i < j) {
-            maxArea = Math.max(maxArea, (j - i) * Math.min(height[i], height[j]));
-            if (height[i] < height[j])
-                    i++;
-            else j--; 
+// Approach-1 Brute force Tc=O(n^2) Sc=O(1)
+// class Solution {
+//     public static int maxArea(int[] height) {
+//         int len = height.length;
+//         int max = 0;
+//         for (int i = 0; i < len - 1; i++) {
+//             for (int j = i + 1; j < len; j++) {
+//                 int area = Math.min(height[i], height[j]) * (j - i);
+//                 max = Math.max(max, area);
+//             }
+//         }
+//         return max;
+//     }  
+// }
+
+// Approach-2 Two Pointers Tc=O(n) Sc=O(1)
+class Solution{
+    public static int maxArea(int[] height){
+       int len = height.length;
+        int left = 0;
+        int right = len - 1;
+        int max = Math.min(height[left], height[right]) * (right - left);
+        while (left < right) {
+            // Move the shorter lines each time
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+            int area=Math.min(height[left], height[right]) * (right - left);
+            // update the max area
+            max = Math.max(max,area);
         }
-            return maxArea;  
-    }  
+
+        return max; 
+    }
 }
-// TC - O(n)
