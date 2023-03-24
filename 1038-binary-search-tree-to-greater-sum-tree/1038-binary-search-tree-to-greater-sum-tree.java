2a8c51172db1747308ba626025e3b1c0ca25571e
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-24 16:42:26 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (21.42%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+     int sum=0;
+    public TreeNode bstToGst(TreeNode root) {
+       
+    if(root==null){
+        return null;
+    }
+    bstToGst(root.right);
+    int a =root.val;
+    sum+=root.val;
+    root.val=sum;
+    bstToGst(root.left);
+    
+    return root;
+}
+}
