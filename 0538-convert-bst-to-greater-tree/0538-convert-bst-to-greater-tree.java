961b8eb5c61122c85379680f4c183c11ba446887
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-24 16:48:01 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (13.71%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -1,3 +1,4 @@
+//Approach-1 Using Recursion Tc=O(n) Sc=O(1) Same as Question==https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
 class Solution {
     int sum=0;
     public TreeNode convertBST(TreeNode root) {
