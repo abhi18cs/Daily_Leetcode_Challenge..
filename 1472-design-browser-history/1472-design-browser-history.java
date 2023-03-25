2ba7d721802941467f85ade09ea10d1cf0cbe1d4
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-25 10:55:19 +0530

    Time: 58 ms (80.45%), Space: 50.4 MB (88.58%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,39 +1,48 @@
 class BrowserHistory {
-    
+    //node class
     public class Node{
         String url;
-        Node next, prev;
-        public Node(String url) {
-            this.url = url;
-            next = null;
-            prev = null;
+        Node prev,next;
+        public Node(String url){//constructor
+            this.url=url;
+            next=null;
+            prev=null;
         }
     }
-    
-    Node head, curr;
+
+    Node head,curr; //instance variable
     public BrowserHistory(String homepage) {
-        head = new Node(homepage);
-        curr = head;
+        head=new Node(homepage);
+        curr=head;
     }
     
     public void visit(String url) {
-        Node node = new Node(url);
-        curr.next = node;
-        node.prev = curr;
-        curr = node;
+        Node node=new Node(url);
+        //adding a new node to end of list
+        curr.next=node;
+        node.prev=curr;
+        curr=node;
     }
     
     public String back(int steps) {
-        while (curr.prev != null && steps-- > 0) {
-            curr = curr.prev;
+        while(curr.prev!=null && steps-- >0){
+            curr=curr.prev;
         }
         return curr.url;
     }
     
     public String forward(int steps) {
-        while (curr.next != null && steps-- > 0) {
-            curr = curr.next;
+        while(curr.next!=null && steps-- >0){
+            curr=curr.next;
         }
         return curr.url;
     }
 }
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
