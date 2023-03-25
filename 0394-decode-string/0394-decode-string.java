1fa300e007bb6df70aea5dd5eaba868a26461ba1
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-25 10:11:01 +0530

    Time: 5 ms (27.07%), Space: 41 MB (22.63%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,37 +1,53 @@
+//The idea is simple we use two stack in this problem one for storing the string and second for storing the integer value.
+
 class Solution {
     public String decodeString(String s) {
-        Stack<String> result=new Stack<>();
-        Stack<Integer> counter=new Stack<>();
-        int i=0;
+        // stack to store integer value
+        Stack<Integer> counter = new Stack();
+        // stack to store String
+        Stack<String> result = new Stack();
+        int index = 0;
         String res="";
-        while(i<s.length()){
-            if(Character.isDigit(s.charAt(i))){
-                int count=0;
-                while(Character.isDigit(s.charAt(i))){
-                count=10*count+(s.charAt(i)-'0');//to convert in to integer
-                    i++;
+
+        while(index<s.length()){
+            // we use Character class here to checkwhether character is digit or not
+            if(Character.isDigit(s.charAt(index))){
+                int count = 0;
+                //If character is digit then loop the character untill you find non-digit character
+                while(Character.isDigit(s.charAt(index))){
+                   //to convert string to integer
+                    count = 10 * count+(s.charAt(index)-'0');
+                    index++;
                 }
                 counter.push(count);
             }
-            else if(s.charAt(i)=='['){
+            else if(s.charAt(index)=='['){
                 // is opening bracket push processed result onto the stack
                 result.push(res);
-                res="";i++;
+                res="";
+                index++;
             }
-            else if(s.charAt(i)==']'){
-                StringBuilder temp=new StringBuilder(result.pop());
-                int count=counter.pop();
-                for(int j=0;j<count;j++){
+            else if(s.charAt(index)==']'){
+                // If closing bracket then pop result from stack and append it to temp 
+                StringBuilder temp = new StringBuilder(result.pop());
+                // pop the counter
+                int count = counter.pop();
+
+                // append the current res count times to temp
+                for(int i=0;i<count;i++){
                     temp.append(res);
                 }
-                res=temp.toString();
-                i++;
+                // assign temp to res
+                res = temp.toString();
+                index++;
             }
             else{
-                res=res+s.charAt(i)+"";
-                i++;
+                // In case you encounter only character of sequence before brackets the compute character String
+                res += s.charAt(index)+"";
+                index++;
             }
         }
+
         return res;
     }
 }
