b0c66e62f08939711a6cd350584cbfb74dbb1fae
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-03-25 10:35:49 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (50.74%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -1,34 +1,59 @@
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if(root==null) return null;
-        
-        if(key<root.val){                            
-            root.left = deleteNode(root.left,key);
+        if(key<root.val){
+            root.left=deleteNode(root.left,key);
             return root;
         }
-        
         else if(key>root.val){
-            root.right = deleteNode(root.right,key);
+            root.right=deleteNode(root.right,key);
             return root;
         }
-        
         else{
-            if(root.left==null){
-                return root.right;
-            }
-            else if(root.right==null){
-                return root.left;
-            }
+            if(root.left==null) return root.right;
+            else if(root.right==null) return root.left; 
             else{
-                TreeNode min = root.right;
-                while(min.left!=null){
-                    min = min.left;
-                }
-                
-                root.val = min.val;
-                root.right = deleteNode(root.right,min.val);
+                TreeNode min =root.right;
+                while(min.left!=null) min=min.left;
+                root.val=min.val;
+                root.right=deleteNode(root.right,root.val);
                 return root;
             }
         }
+        
     }
 }
+// class Solution {
+//     public TreeNode deleteNode(TreeNode root, int key) {
+//         if(root==null) return null;
+        
+//         if(key<root.val){                            
+//             root.left = deleteNode(root.left,key);
+//             return root;
+//         }
+        
+//         else if(key>root.val){
+//             root.right = deleteNode(root.right,key);
+//             return root;
+//         }
+        
+//         else{
+//             if(root.left==null){
+//                 return root.right;
+//             }
+//             else if(root.right==null){
+//                 return root.left;
+//             }
+//             else{
+//                 TreeNode min = root.right;
+//                 while(min.left!=null){
+//                     min = min.left;
+//                 }
+                
+//                 root.val = min.val;
+//                 root.right = deleteNode(root.right,min.val);
+//                 return root;
+//             }
+//         }
+//     }
+// }
