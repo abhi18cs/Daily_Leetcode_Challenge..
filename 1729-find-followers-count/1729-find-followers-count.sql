41bb604d318b5aa4486855f0aa9113601b67a36e
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-06 08:57:17 +0530

    Time: 1231 ms (21.26%), Space: 0B (100.00%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,5 +1,3 @@
-select user_id ,count(distinct follower_id) as followers_count from followers group by user_id;
-
-# SELECT user_id, COUNT(DISTINCT follower_id) AS followers_count
-# FROM followers
-# GROUP BY user_id
+SELECT user_id, COUNT(DISTINCT follower_id) AS followers_count
+FROM followers
+GROUP BY user_id
