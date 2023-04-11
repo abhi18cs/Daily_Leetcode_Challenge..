d7298c62e832f307cd95d17696089be2235c0aa6
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-11 15:57:22 +0530

    Time: 2 ms (86.32%), Space: 59 MB (76.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -12,23 +12,36 @@
 // }
 
 //optimized approach tc=O(n) SC=O(1) Sliding window approach
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int lsf = Integer.MAX_VALUE; // least so far
+//         int op = 0; // overall profit
+//         int pist = 0; // profit if sold today
+//         for(int i = 0; i < prices.length; i++){
+// // if we found new buy value which is more smaller then previous one
+//             if(prices[i] < lsf){ 
+//                 lsf = prices[i]; // update our least so far
+//             }
+// // calculating profit if sold today by, Buy - sell            
+//             pist = prices[i] - lsf; 
+// // if pist is more then our previous overall profit
+//             if(op < pist){ 
+//                 op = pist; // update overall profit
+//             }
+//         }
+//         return op; // return op 
+//     }
+// }
+
 class Solution {
-    public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE; // least so far
-        int op = 0; // overall profit
-        int pist = 0; // profit if sold today
-        for(int i = 0; i < prices.length; i++){
-// if we found new buy value which is more smaller then previous one
-            if(prices[i] < lsf){ 
-                lsf = prices[i]; // update our least so far
-            }
-// calculating profit if sold today by, Buy - sell            
-            pist = prices[i] - lsf; 
-// if pist is more then our previous overall profit
-            if(op < pist){ 
-                op = pist; // update overall profit
-            }
-        }
-        return op; // return op 
-    }
+     public int maxProfit(int[] prices) {
+         int min=prices[0];
+         int maxProfit=0;
+         for(int i=1;i<prices.length;i++){
+             int cost =prices[i]-min;
+             maxProfit=Math.max(cost,maxProfit);
+             min=Math.min(min,prices[i]);
+         }
+         return maxProfit;
+     }
 }
