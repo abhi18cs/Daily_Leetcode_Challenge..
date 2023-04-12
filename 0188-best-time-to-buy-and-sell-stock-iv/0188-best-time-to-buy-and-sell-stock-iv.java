4fcbd076124a60ea9471960199689fcaa6a5b106
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-12 19:03:36 +0530

    Time: 1 ms (100.00%), Space: 40.5 MB (88.55%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,16 @@
 class Solution {
-    int[][][] dp;
-    public int maxProfit(int c, int[] prices) {
-        int n = prices.length;
-        dp = new int[n][2][c+1];  // initialize the 3-dimensional dp array
-        
-        return find(0, 1, c, prices);
-    }
-    
-    private int find(int i, int buy, int c, int[] arr){
-        if(i == arr.length || c == 0)  return 0;
-        if(dp[i][buy][c] != 0)  return dp[i][buy][c];  
-        // if the result of this subproblem has already been computed, return it
-        
-        if(buy == 1){
-			return dp[i][buy][c] = Math.max(-arr[i] + find(i+1, 0, c, arr), find(i+1, 1, c, arr));     // Here we have choice to whether choose that element or not, and after that, the part giving maximum profit after buying(be that negative) is taken
-        }
-        else{
-            return dp[i][buy][c] = Math.max(arr[i] + find(i+1, 1, c-1, arr), find(i+1, 0, c, arr));   // Same as above but the only change is that profit added is positive because we are selling this time.
-        }
-    }
+    public int maxProfit(int k, int[] prices) {
+       int profit[] = new int[k];
+       int cost[] = new int[k];
+       Arrays.fill(cost, Integer.MAX_VALUE);
+       for(int currPrice : prices){
+            cost[0] = Math.min(cost[0], currPrice);
+            profit[0] = Math.max(profit[0], currPrice-cost[0]);
+			for(int j = 1;  j < k; j++){
+                cost[j] = Math.min(cost[j], currPrice - profit[j-1]);
+                profit[j] = Math.max(profit[j], currPrice - cost[j]);
+            }
+       }
+       return profit[k-1];
+   }
 }
