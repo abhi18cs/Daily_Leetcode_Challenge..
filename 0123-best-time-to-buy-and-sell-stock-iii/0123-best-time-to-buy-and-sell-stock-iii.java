a8df3229d60bb70a07daeb84303f1acbf5e5ddd2
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-13 07:18:02 +0530

    Time: 79 ms (36.55%), Space: 64.6 MB (30.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -23,36 +23,58 @@
 // }
 
 
-//Approach-2 USing Recursion+Memoization(TopDownApproach) Tc=O(n*2*3) Sc=O(n*2*3)+O(n)
-class Solution {    
-    public int maxProfit(int[] prices) {
-       int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<2; j++){
-                Arrays.fill(dp[i][j], -1);      
-            }
-        } 
-        //return solve(0, prices, 1, dp, n, 2);
-         return solve(0,prices,1,dp,n,2);
-    }
+ //Approach-2 USing Recursion+Memoization(TopDownApproach) Tc=O(n*2*3) Sc=O(n*2*3)+O(n)
+// class Solution {    
+//     public int maxProfit(int[] prices) {
+//        int n = prices.length;
+//         int[][][] dp = new int[n][2][3];
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<2; j++){
+//                 Arrays.fill(dp[i][j], -1);      
+//             }
+//         } 
+//         //return solve(0, prices, 1, dp, n, 2);
+//          return solve(0,prices,1,dp,n,2);
+//     }
     
-    public int solve(int index,int[] prices,int buy,int[][][] dp,int n,int k){
-        if(index==prices.length) return 0;
-        if(k==0) return 0;
-        if(dp[index][buy][k]!=-1) return dp[index][buy][k];
-        int profit=0;
-        //buy=1(Allowed),buy=0(Not allowed)
-        if(buy==1){
-            profit=Math.max(-prices[index]+solve(index+1,prices,0,dp,n,k),
-                            0+solve(index+1,prices,1,dp,n,k));
-        }
-        else{
-            profit=Math.max(prices[index]+solve(index+1,prices,1,dp,n,k-1),
-                            0+solve(index+1,prices,0,dp,n,k));
-        }
-        dp[index][buy][k]=profit;
-        return dp[index][buy][k];
+//     public int solve(int index,int[] prices,int buy,int[][][] dp,int n,int k){
+//         if(index==prices.length) return 0;
+//         if(k==0) return 0;
+//         if(dp[index][buy][k]!=-1) return dp[index][buy][k];
+//         int profit=0;
+//         //buy=1(Allowed),buy=0(Not allowed)
+//         if(buy==1){
+//             profit=Math.max(-prices[index]+solve(index+1,prices,0,dp,n,k),
+//                             0+solve(index+1,prices,1,dp,n,k));
+//         }
+//         else{
+//             profit=Math.max(prices[index]+solve(index+1,prices,1,dp,n,k-1),
+//                             0+solve(index+1,prices,0,dp,n,k));
+//         }
+//         dp[index][buy][k]=profit;
+//         return dp[index][buy][k];
+//     }
+// }
+
+
+//Approach-3 USing Tabulation Method(BottomUpApproach) Tc=O(n*2*3) Sc=O(n*2*3)+O(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n= prices.length;
+        int dp[][][]= new int[n+1][2][3];
+          for(int idx = n-1 ;idx>=0 ;idx--){
+              for(int buy =0;buy<=1 ;buy++){
+                  for(int cap =1 ;cap<=2;cap++){
+                      if(buy==1){
+                          dp[idx][buy][cap] = Math.max(-prices[idx] +dp[idx+1][0][cap] , dp[idx+1][1][cap]);
+                      }else{
+                          dp[idx][buy][cap] = Math.max(prices[idx]+dp[idx+1][1][cap-1] , dp[idx+1][0][cap]);
+                      }
+                  }
+              }
+          }
+        return dp[0][1][2];
+        
     }
 }
 
