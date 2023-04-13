40b60d14a3f3eaa00968fce85e7e08c975abb3ae
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-13 07:20:54 +0530

    Time: 84 ms (31.49%), Space: 64.7 MB (30.31%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -60,19 +60,23 @@
 //Approach-3 USing Tabulation Method(BottomUpApproach) Tc=O(n*2*3) Sc=O(n*2*3)+O(n)
 class Solution {
     public int maxProfit(int[] prices) {
-        int n= prices.length;
-        int dp[][][]= new int[n+1][2][3];
-          for(int idx = n-1 ;idx>=0 ;idx--){
-              for(int buy =0;buy<=1 ;buy++){
-                  for(int cap =1 ;cap<=2;cap++){
-                      if(buy==1){
-                          dp[idx][buy][cap] = Math.max(-prices[idx] +dp[idx+1][0][cap] , dp[idx+1][1][cap]);
-                      }else{
-                          dp[idx][buy][cap] = Math.max(prices[idx]+dp[idx+1][1][cap-1] , dp[idx+1][0][cap]);
-                      }
-                  }
-              }
-          }
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][3];
+        for(int i = n-1; i >= 0; i--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int cap = 1; cap < 3; cap++){
+                    if(i==n) return 0;
+                    if(buy == 1){
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap]
+                                                            , 0 + dp[i+1][1][cap]);                     
+                        }
+                    else{
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1]
+                                                            , 0 + dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
         return dp[0][1][2];
         
     }
