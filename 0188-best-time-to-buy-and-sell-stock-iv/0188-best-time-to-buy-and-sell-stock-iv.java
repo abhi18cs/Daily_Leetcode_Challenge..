f972956fac9252e3c9bf3f79ac355408f7212201
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-04-13 06:52:10 +0530

    Time: 9 ms (36.77%), Space: 41.9 MB (74.23%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,30 +1,58 @@
+//Approach-1 Recursive TLE Tc=O(2^n) Sc=O(n)
+// class Solution{
+//     public int maxProfit(int k,int[] prices){
+//         return solve(0,1,prices,k);
+//     }
+//     public int solve(int index,int buy,int[] prices,int limit){
+//         if(index==prices.length) return 0;
+//         if(limit==0) return 0;
+//         int profit=0;
+//         //buy=1(Allowed),buy=0(Not allowed)
+//         if(buy==1){
+//             buy=-prices[index]+solve(index+1,0,prices,limit);
+//             int skip=0+solve(index+1,1,prices,limit);
+//             profit=Math.max(buy,skip);
+//         }
+//         else{
+//             int sell=prices[index]+solve(index+1,1,prices,limit-1);
+//             int skip=0+solve(index+1,0,prices,limit);
+//             profit=Math.max(sell,skip);
+//         }
+//         return profit;
+//     }
+// }
 
-class Solution { 
-   int[][][]dp;
+
+class Solution {  
+    int[][][]dp;
     public int maxProfit(int k,int[] prices) {
        int n = prices.length;
         dp = new int[n][2][k+1];
-       
+         
         //return solve(0, prices, 1, dp, n, 2);
-         return solve(0,1,k,prices);
+         return solve(0,prices,1,dp,n,k);
     }
     
-    public int solve(int index,int buy,int k,int[] prices){
-        if(index==prices.length || k==0) return 0;
+    public int solve(int index,int[] prices,int buy,int[][][] dp,int n,int k){
+        if(index==prices.length) return 0;
+        if(k==0) return 0;
         if(dp[index][buy][k]!=0) return dp[index][buy][k];
-       // int profit=0;
+        int profit=0;
         //buy=1(Allowed),buy=0(Not allowed)
         if(buy==1){
-          return dp[index][buy][k]=Math.max(-prices[index]+ solve(index+1,0,k,prices),
-                            solve(index+1,1,k,prices));
+            profit=Math.max(-prices[index]+solve(index+1,prices,0,dp,n,k),
+                            0+solve(index+1,prices,1,dp,n,k));
         }
         else{
-            return dp[index][buy][k]=Math.max(prices[index]+solve(index+1,1,k-1,prices),
-                            solve(index+1,0,k,prices));
+            profit=Math.max(prices[index]+solve(index+1,prices,1,dp,n,k-1),
+                            0+solve(index+1,prices,0,dp,n,k));
         }
-        
+        dp[index][buy][k]=profit;
+        return dp[index][buy][k];
     }
 }
+
+
 
 // class Solution {
 //     int[][][] dp;
@@ -37,9 +65,9 @@ class Solution {
     
 //     private int find(int i, int buy, int c, int[] arr){
 //         if(i == arr.length || c == 0)  return 0;
-// //if the result of this subproblem has already been computed, return it
+// if the result of this subproblem has already been computed, return it
 //         if(dp[i][buy][c] != 0)  return dp[i][buy][c];  
-        
+//         
         
 //         if(buy == 1){
 // 			return dp[i][buy][c] = Math.max(-arr[i] + find(i+1, 0, c, arr), find(i+1, 1, c, arr));     // Here we have choice to whether choose that element or not, and after that, the part giving maximum profit after buying(be that negative) is taken
