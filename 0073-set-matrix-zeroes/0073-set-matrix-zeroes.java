b561c3b901667a1fa1120188899875cfb9b8f89b
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-14 11:29:59 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (39.10%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,26 +1,26 @@
 //https://leetcode.com/problems/set-matrix-zeroes/solutions/2525398/all-approaches-from-brute-force-to-optimal-with-easy-explanation/?orderBy=most_votes&languageTags=java
 
-//Tc=O((mn)*(m+n)) Sc=O(mn)
+//Brute Force Tc=O((mn)*(m+n)) Sc=O(mn)
 class Solution {
     public void setZeroes(int[][] matrix) {
-    int m=matrix.length;
-    int n=matrix[0].length;
-    int[] row = new int[m];
-    int[] col = new int[n];
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]==0){
-                row[i]=1;
-                col[j]=1;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1||col[j]==1){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(row[i]==1||col[j]==1){
-                matrix[i][j]=0;
-            }
-        }
-    }
-}
 }
