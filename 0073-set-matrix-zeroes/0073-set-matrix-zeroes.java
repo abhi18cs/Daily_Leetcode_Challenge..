4e117c2552648fe3a952568cd77ac0661a3bde41
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-14 11:37:06 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (20.51%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,15 +3,15 @@
 //Brute Force Tc=O((mn)*(m+n)) Sc=O(mn)
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int[] row = new int[m];
+        int m=matrix.length;//no of rows
+        int n=matrix[0].length;// no of col
+        int[] row = new int[m];//new matrix of same length
         int[] col = new int[n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    row[i]=1;// traverse and check the ele is zero if it is then just                                       simply place 1 at that place
+                  col[j]=1;
                 }
             }
         }
