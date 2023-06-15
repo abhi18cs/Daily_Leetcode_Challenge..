e2004b7e96e394e642295b53d22085013dee0744
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-15 10:45:51 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (52.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,27 +1,47 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //in this que for clockwise we do column reverse and for anticlockwise row reverse now after reversing we do the transpose of the matrix
-        //1.Transpose of matrix
-        for(int r=0;r<matrix.length;r++){
-            for(int c=0;c<r;c++){
-                int temp=matrix[r][c];
-                matrix[r][c]=matrix[c][r];
-                matrix[c][r]=temp;
+        int n = matrix.length;
+        int[][] rotated = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                rotated[j][n - i - 1] = matrix[i][j];
             }
         }
-            
         
-        //2.we here reverse the column bcoz it is c.w
-        int fc=0;  //firstcol
-        int lc=matrix[0].length-1;  //lastcol
-         while(fc<lc){
-             //now here we are swapping the first
-             for(int r=0;r<matrix.length;r++){
-                 int temp=matrix[r][fc];
-                 matrix[r][fc]=matrix[r][lc];
-                 matrix[r][lc]=temp;
-             }
-             fc++; lc--;
-         }
+        // Copy the rotated matrix back to the original matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = rotated[i][j];
+            }
+        }
     }
 }
+
+// class Solution {
+//     public void rotate(int[][] matrix) {
+//         //in this que for clockwise we do column reverse and for anticlockwise row reverse now after reversing we do the transpose of the matrix
+//         //1.Transpose of matrix
+//         for(int r=0;r<matrix.length;r++){
+//             for(int c=0;c<r;c++){
+//                 int temp=matrix[r][c];
+//                 matrix[r][c]=matrix[c][r];
+//                 matrix[c][r]=temp;
+//             }
+//         }
+            
+        
+//         //2.we here reverse the column bcoz it is c.w
+//         int fc=0;  //firstcol
+//         int lc=matrix[0].length-1;  //lastcol
+//          while(fc<lc){
+//              //now here we are swapping the first
+//              for(int r=0;r<matrix.length;r++){
+//                  int temp=matrix[r][fc];
+//                  matrix[r][fc]=matrix[r][lc];
+//                  matrix[r][lc]=temp;
+//              }
+//              fc++; lc--;
+//          }
+//     }
+// }
