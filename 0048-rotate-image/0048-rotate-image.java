004452b398e00a5e481d8577e3417b2b82771117
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-15 10:52:21 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (67.60%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,47 +1,44 @@
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int[][] rotated = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rotated[j][n - i - 1] = matrix[i][j];
-            }
-        }
-        
-        // Copy the rotated matrix back to the original matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = rotated[i][j];
-            }
-        }
-    }
-}
+//Brute Force Approach Tc=O(N*N) to linearly iterate and put it into some other matrix.
+//Sc=O(N*N) to copy it into some other matrix.
 
 // class Solution {
 //     public void rotate(int[][] matrix) {
-//         //in this que for clockwise we do column reverse and for anticlockwise row reverse now after reversing we do the transpose of the matrix
-//         //1.Transpose of matrix
-//         for(int r=0;r<matrix.length;r++){
-//             for(int c=0;c<r;c++){
-//                 int temp=matrix[r][c];
-//                 matrix[r][c]=matrix[c][r];
-//                 matrix[c][r]=temp;
+//         int n = matrix.length;
+//         int[][] rotated = new int[n][n];//make a new matrix to copy the data
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 rotated[j][n - i - 1] = matrix[i][j];
 //             }
 //         }
-            
-        
-//         //2.we here reverse the column bcoz it is c.w
-//         int fc=0;  //firstcol
-//         int lc=matrix[0].length-1;  //lastcol
-//          while(fc<lc){
-//              //now here we are swapping the first
-//              for(int r=0;r<matrix.length;r++){
-//                  int temp=matrix[r][fc];
-//                  matrix[r][fc]=matrix[r][lc];
-//                  matrix[r][lc]=temp;
-//              }
-//              fc++; lc--;
-//          }
+//         // Copy the rotated matrix back to the original matrix
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 matrix[i][j] = rotated[i][j];
+//             }
+//         }
 //     }
 // }
+
+//Optimal Approach Tc=O(n*n) Sc=O()
+class Solution {
+    public void rotate(int[][] matrix) {
+        //in this que for clockwise we do column reverse and for anticlockwise row reverse now after reversing we do the transpose of the matrix
+        //1.Transpose of matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+    }
