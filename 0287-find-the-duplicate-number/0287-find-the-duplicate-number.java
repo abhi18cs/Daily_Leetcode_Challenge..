1bee0bd2bb9dbce0bef6454a92da9f87c83b8a8d
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-15 18:26:57 +0530

    Time: 4 ms (88.14%), Space: 56.5 MB (80.48%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,14 +1,14 @@
 //Brute Force Using Sorting TC=O(nlogn+n) SC=O(1)
-class Solution{
-    public int findDuplicate(int[] nums){
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==nums[i+1])
-                return nums[i];
-        }
-        return -1;
-    }
-}
+// class Solution{
+//     public int findDuplicate(int[] nums){
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==nums[i+1])
+//                 return nums[i];
+//         }
+//         return -1;
+//     }
+// }
 
 //Better Approach Tc=O(n) Sc=O(n) using hashmap
 // class Solution {
@@ -32,7 +32,25 @@ class Solution{
 //     }
 // }
 
-// TC=O(N) SC=O(1) Visited
+//Optimal Approach USing LinkedList Cycle Method Tc=O(N) Sc=O(1) 
+class Solution{
+    public static int findDuplicate(int[] nums){
+        int slow=nums[0],fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+}
+
+
+// Optimal Approach TC=O(N) SC=O(1) Visited
 // class Solution{
 //     public static int findDuplicate(int[] nums) {
 //         int len = nums.length;
