b6e8317d11498c09de1c7d6bb4b3f8791c1bcf8c
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-15 09:32:02 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (25.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,17 +2,17 @@
 //Better Approach Tc=O(N+N) Sc=O(1)
 class Solution {
     public void sortColors(int[] nums) {
+        int n=nums.length;
         int count0 = 0, count1 = 0, count2 = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {count0++;}
             if (nums[i] == 1) {count1++;}
             if (nums[i] == 2) {count2++;}
-            }
-        for(int i = 0; i < nums.length; i++) {
-            if (i < count0) {nums[i] = 0;}
-            else if (i < count0 + count1) {nums[i] = 1;}
-            else {nums[i] = 2;}
         }
+    for (int i = 0; i < count0; i++) nums[i] =0; // replacing 0's
+    for (int i = count0; i < count0 + count1; i++) nums[i] =1; // replacing 1's
+    for (int i = count0 + count1; i < n; i++) nums[i] =2; // replacing 2's
+    
     }
 }
 
