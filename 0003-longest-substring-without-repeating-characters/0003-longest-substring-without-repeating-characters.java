a5f774a5c32b73851ad1e120041c70538266848d
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-17 17:58:23 +0530

    Time: 6 ms (87.02%), Space: 43.7 MB (35.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,43 +1,40 @@
-//Approach-1 
+//Approach-1 Tc=O(n2) Sc=O(n)
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if (s.length() <= 1)
+//             return s.length();
+//         int maxans = 0;
+//         for (int i = 0; i < s.length(); i++) {
+//             Set<Character> se = new HashSet<>();
+//             int j = i;
+//             while (j < s.length() && !se.contains(s.charAt(j))) {
+//                 se.add(s.charAt(j));
+//                 j++;
+//             }
+//             maxans = Math.max(maxans, j - i);
+//         }
+//         return maxans;
+//     }
+// }
+
+//Approach-2 Better Approach Usnig 2 pointers Tc=O(n2) Sc=O(n)
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() <= 1)
-            return s.length();
-        
-        int maxans = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            Set<Character> se = new HashSet<>();
-            int j = i;
-            while (j < s.length() && !se.contains(s.charAt(j))) {
-                se.add(s.charAt(j));
-                j++;
+       if(s.length()==0) return 0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        int max=0;
+        //2 pointers
+        for(int i=0,j=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                j=Math.max(j,map.get(s.charAt(i))+1);
             }
-            maxans = Math.max(maxans, j - i);
+            map.put(s.charAt(i),i);
+            max=Math.max(max,i-j+1);
         }
-        
-        return maxans;
+        return max;
     }
 }
 
-//brute forece usnig 2 pointers
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//        if(s.length()==0) return 0;
-//         HashMap<Character,Integer> map=new HashMap<>();
-//         int max=0;
-//         //2 pointers
-//         for(int i=0,j=0;i<s.length();i++){
-//             if(map.containsKey(s.charAt(i))){
-//                 j=Math.max(j,map.get(s.charAt(i))+1);
-//             }
-//             map.put(s.charAt(i),i);
-//             max=Math.max(max,i-j+1);
-//         }
-//         return max;
-//     }
-// }
-// //TC=O(n^2)
 
 //appraoch-2 using hashset tc=O(n),sc=O(n) 
 //Sliding windows with hashet check duplicated character
