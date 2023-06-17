5a0636224bd435951ecd66c895abf4b96d1b4021
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-17 17:56:13 +0530

    Time: 140 ms (11.45%), Space: 44.4 MB (8.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,3 +1,25 @@
+//Approach-1 
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() <= 1)
+            return s.length();
+        
+        int maxans = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> se = new HashSet<>();
+            int j = i;
+            while (j < s.length() && !se.contains(s.charAt(j))) {
+                se.add(s.charAt(j));
+                j++;
+            }
+            maxans = Math.max(maxans, j - i);
+        }
+        
+        return maxans;
+    }
+}
+
 //brute forece usnig 2 pointers
 // class Solution {
 //     public int lengthOfLongestSubstring(String s) {
@@ -19,17 +41,17 @@
 
 //appraoch-2 using hashset tc=O(n),sc=O(n) 
 //Sliding windows with hashet check duplicated character
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-    int i=0,j=0,max=0;
-    HashSet<Character> set=new HashSet<>();
-    while(j<s.length()){
-        if(!set.contains(s.charAt(j))){
-            set.add(s.charAt(j++));
-            max=Math.max(max,set.size());
-        }
-        else set.remove(s.charAt(i++));
-    }
-    return max;
-    }
-}
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//     int i=0,j=0,max=0;
+//     HashSet<Character> set=new HashSet<>();
+//     while(j<s.length()){
+//         if(!set.contains(s.charAt(j))){
+//             set.add(s.charAt(j++));
+//             max=Math.max(max,set.size());
+//         }
+//         else set.remove(s.charAt(i++));
+//     }
+//     return max;
+//     }
+// }
