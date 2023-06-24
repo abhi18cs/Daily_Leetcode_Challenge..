424e3ef4e547aad6d6dce5dc102ada0ad6ff57ae
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-24 19:04:41 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (49.77%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -1,35 +1,35 @@
 //Appraoch-1 Using HashSet Tc=O(n) Sc=O(n)
-class Solution {
-    public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> st=new HashSet<>();
-            while(head != null) {
-                if(st.contains(head)) return head;
-                st.add(head);
-                head = head.next;
-            }
-            return null;
-    }
-}
-
-// //tc=O(n) sc=O(1)
-// public class Solution {
+// class Solution {
 //     public ListNode detectCycle(ListNode head) {
-//         if(head==null || head.next==null) return null;
-//         ListNode slow=head;
-//         ListNode fast=head;
-//         ListNode entry=head;
-//         while(fast.next!=null && fast.next.next!=null){
-//             slow=slow.next;
-//             fast=fast.next.next;
-//             if(slow==fast){
-//                 while(slow!=entry){
-//                     slow=slow.next;
-//                     entry=entry.next;
-//                 }
-//                 return entry;
+//         HashSet<ListNode> st=new HashSet<>();
+//             while(head != null) {
+//                 if(st.contains(head)) return head;
+//                 st.add(head);
+//                 head = head.next;
 //             }
-//         }
-//         return null;
+//             return null;
 //     }
 // }
+
+//Appraoch-2 Using Slow and Fast Pointers Tc=O(n) Sc=O(n)
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null || head.next==null) return null;
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode entry=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                while(slow!=entry){
+                    slow=slow.next;
+                    entry=entry.next;
+                }
+                return entry;
+            }
+        }
+        return null;
+    }
+}
 
