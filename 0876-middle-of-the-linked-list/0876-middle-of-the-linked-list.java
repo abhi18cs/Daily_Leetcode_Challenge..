2eac8f8030c069318a23c35c948d3194d1b20556
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-06-24 16:40:50 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (34.06%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,13 +1,15 @@
-//Approach-1 Using Recursion Tc=O(n) Sc=O(1) We can traverse through the Linked List while maintaining a count of nodes let’s say in variable n, and then traversing for 2nd time for n/2 nodes to get to the middle of the list.
+//Approach-1 Using Recursion Tc=O(n) Sc=O(1) 
 class Solution {
     public ListNode middleNode(ListNode head) {
         int n=0;
         ListNode temp=head;
+        //We can traverse through the Linked List while maintaining a count of nodes let’s say in variable n.
         while(temp!=null){
             n++;
             temp=temp.next;
         }
         temp=head;
+        //then traversing for 2nd time for n/2 nodes to get to the middle of the list
         for(int i=0;i<n/2;i++){
             temp=temp.next;
         }
@@ -15,7 +17,7 @@ class Solution {
     }
 }
 
-//Approach-2 Using Recursion Tc=O(n) Sc=O(1)
+//Approach-2 Using Recursion Tc=O(n) Sc=O(1) Tortoise-Hare-Approach
 // class Solution {
 //     public ListNode middleNode(ListNode head) {
 //         ListNode slow=head,fast=head;
