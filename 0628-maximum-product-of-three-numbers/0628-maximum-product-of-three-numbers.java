2844320ebf6aa386a4b37cd4b5f07ec042e5df6c
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-07-08 10:01:09 +0530

    Time: 13 ms (74.17%), Space: 45.1 MB (47.86%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,3 +1,6 @@
+//Approach-1 UsingSorting Tc=O(n) Sc=O(1);
+//In "case1," we consider the possibility of negative numbers impacting the maximum product calculation. We multiply the two smallest numbers (nums[0] and nums[1]) in the sorted array by the largest number (nums[nums.length-1]). This accounts for the scenario where multiplying two negative numbers results in a positive product.In "case2," we calculate the product of the three largest numbers in the sorted array (nums[nums.length-1], nums[nums.length-2], and nums[nums.length-3]).By comparing the products calculated in "case1" and "case2," we determine the maximum product and assign it to the variable ans.
+
 class Solution {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
