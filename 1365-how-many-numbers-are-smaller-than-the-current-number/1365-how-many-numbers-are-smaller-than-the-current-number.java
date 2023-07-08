2a67119ab278a79aba02591efb33473b32e0ef08
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-07-08 09:10:20 +0530

    Time: 1 ms (99.87%), Space: 43.4 MB (79.83%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,31 @@
 //Approach-1 BruteForce Traversing through each element Tc=O(n^2) Sc=O(n)
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+//         int arr[]=new int [nums.length];
+//         int count=0;
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=0;j<nums.length;j++){
+//                 if(j!=i && nums[i]>nums[j]) count++;
+//             }
+//             arr[i]=count;
+//             count=0;
+//         }
+//         return arr;
+//     }
+// }
+//Approach-2 OptimiziedApproach count the number occurrences of each number Tc=O(n) Sc=O(n)
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int arr[]=new int [nums.length];
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(j!=i && nums[i]>nums[j]) count++;
-            }
-            arr[i]=count;
-            count=0;
-        }
-        return arr;
+        int[] bucket = new int[102];
+        for(int i=0; i<nums.length; i++)
+            bucket[nums[i]+1]++;
+			
+        for(int i=0; i<101; i++) 
+            bucket[i+1] += bucket[i];
+			
+        for(int i=0; i<nums.length; i++)
+            nums[i] = bucket[nums[i]];
+			
+        return nums;
     }
 }
