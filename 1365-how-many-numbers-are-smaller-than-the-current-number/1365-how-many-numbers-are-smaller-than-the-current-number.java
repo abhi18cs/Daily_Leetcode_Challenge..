f4e46a1b1844d1149533a6db708217106f675b50
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-07-08 08:54:10 +0530

    Time: 15 ms (34.60%), Space: 43.6 MB (75.34%) - LeetHub

---
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,15 @@
+//Approach-1 BruteForce Traversing through each element Tc=O(n^2) Sc=O(n)
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int arr[]=new int [nums.length];
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]>nums[j]) count++;
+            }
+            arr[i]=count;
+            count=0;
+        }
+        return arr;
+    }
+}
