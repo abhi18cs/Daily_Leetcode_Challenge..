850677675ccef74965f62d1c8257ddee510a76a9
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-08-21 09:46:16 +0530

    Time: 2988 ms (6.03%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -39,7 +39,7 @@ select st.student_id, st.student_name, sb.subject_name, count(e.subject_name) as
 from Students st
 join Subjects sb
 left join
-examinations e
+Examinations e
 on e.student_id = st.student_id
 and e.subject_name = sb.subject_name
 group by st.student_id, sb.subject_name
