a24051c3a676af397c52e0ef25296f8644e807d8
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-08-29 09:47:06 +0530

    Time: 8 ms (98.65%), Space: 44.6 MB (27.27%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -5,7 +5,9 @@ class Solution {
         int hour = 0;
 
         for(int i=0; i<customers.length(); i++){
-            profit = customers.charAt(i)=='Y' ? profit+1 : profit-1;
+            if(customers.charAt(i)=='Y')
+                profit=profit+1;
+            else profit=profit-1;
             if(profit>maxProfit){
                 maxProfit = profit;
                 hour = i+1;
