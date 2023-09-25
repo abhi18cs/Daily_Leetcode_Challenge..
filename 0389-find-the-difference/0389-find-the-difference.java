11a0a5831425f062ddfb5fe13c62a5ac6b7ca4a2
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-09-25 09:23:06 +0530

    Time: 2 ms (68.98%), Space: 41 MB (29.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,12 +1,19 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int k = 0;
-        int j = 0;
-        for (int i = 0; i<s.length(); i++){
-            k+=s.charAt(i);
-            j+=t.charAt(i);
+        // Initialize variables to store the sum of ASCII values
+        int k = 0; // For string s
+        int j = 0; // For string t
+        
+        // Iterate through the characters in string s
+        for (int i = 0; i < s.length(); i++) {
+            k += s.charAt(i); // Add the ASCII value of s[i] to k
+            j += t.charAt(i); // Add the ASCII value of t[i] to j
         }
-        j+=t.charAt(s.length());
-        return (char) (j-k);
+        
+        // Add the ASCII value of the extra character in t
+        j += t.charAt(s.length());
+        
+        // Calculate the difference in ASCII values and convert it to a character
+        return (char) (j - k);
     }
 }
