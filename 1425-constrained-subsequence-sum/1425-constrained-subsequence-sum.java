d25811f64444dbdf2abaecc3ac2c1c28f6940ee7
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-10-21 08:20:06 +0530

    Time: 37 ms (62.20%), Space: 57 MB (40.94%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int constrainedSubsetSum(int[] arr, int k) {
+        int n = arr.length;
+        Deque<Integer> deque = new LinkedList<>();
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+			int max = Math.max(0, deque.isEmpty() ? 0 : arr[deque.peekFirst()]);
+            arr[i] += max;
+            ans = Math.max(ans, arr[i]);
+            while (!deque.isEmpty() && arr[i] >= arr[deque.peekLast()]) { // If dp[i] >= deque.peekLast() -> Can discard the tail since it's useless
+                deque.pollLast();
+            }
+            deque.addLast(i);
+            if (i - deque.peekFirst() + 1 > k) { // remove the last element of range k
+                deque.removeFirst();
+            }
+        }
+        return ans;
+    }
+}
