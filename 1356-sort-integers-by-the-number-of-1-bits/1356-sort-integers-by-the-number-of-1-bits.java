b714e5349e2949331b54e764dbef29d22960c46f
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2023-10-30 11:44:34 +0530

    Time: 3 ms (99.13%), Space: 43.7 MB (53.28%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            arr[i]+=10001*Integer.bitCount(arr[i]);
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            arr[i]%=10001;
+        }
+        return arr;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
