7bcd69c025614020893057701c030233bdc570d5
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2024-03-02 09:08:14 +0530

    Time: 2 ms (51.82%), Space: 47 MB (61.58%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -0,0 +1,38 @@
+// //brute force tc=O(nlogn) sc=O(n)
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         int ans[]=new int[nums.length];
+//         for(int i=0;i<nums.length;i++){
+//             ans[i]=nums[i]*nums[i];
+//         }
+//         Arrays.sort(ans);
+//         return ans;
+//     }
+// }
+//Using 2 Pointers
+//tc=O() sc=O(n)
+class Solution{
+    public int[] sortedSquares(int[] A) {
+        //result array
+        int[] res = new int[A.length];
+        //pointers for left and right
+        int lo = 0; int hi = A.length - 1;
+        //iterate from n to 0
+        for (int i = A.length - 1; i >= 0; i--) {
+            //check if abs left is less than or equal to abs right
+            if (Math.abs(A[lo]) >= Math.abs(A[hi])) {
+                //add left squared to result array
+                res[i] = A[lo] * A[lo];
+                //increment left pointer
+                lo++;
+            } else {
+                //add right squared to result array
+                res[i] = A[hi] * A[hi];
+                //decrement right pointer
+                hi--;
+            }
+        }
+        //result
+        return res;
+    }
+}
