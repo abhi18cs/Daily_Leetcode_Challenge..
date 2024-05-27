7b9bf3ad5e10f712604972c89496a0d7469af791
Abhishek Srivastava <96434342+abhi18cs@users.noreply.github.com>
2024-05-27 08:55:03 +0530

    Time: 1 ms (78.32%), Space: 40.9 MB (83.20%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,37 @@
+//Brute force
+// class Solution {
+//     public int specialArray(int[] nums) {
+//         for(int i=1;i<1001;i++){
+//             int count=0;
+//             for(int num:nums){
+//                 if(num>=i) count++;
+//             }
+//             if(i==count) return i;
+//         }
+//         return -1;
+//     }
+// }//TC=O(n2)
+class Solution {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        // binary search to find the first position that
+        // nums[i] >= len - i and nums[i - 1] < len - i
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= len - mid) {
+                // treat index 0 to avoid IndexOutOfBoundError
+                if (mid == 0 || nums[mid - 1] < len - mid) { // correct index
+                    return len - mid;
+                } else { // not the first position, shrink right bound
+                    right = mid - 1;
+                }
+            } else { // otherwisem, shrink the left bound to increase nums[mid]
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
